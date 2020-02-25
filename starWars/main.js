bef7642e04b72169ccb644c0a92c98d1829af8e5
PhatSharkBait <10748722@cs715b-25.ad.uvu.edu>
2020-02-25 12:45:09 -0700

    linking star wars apis

--- starWars/main.js
+++ starWars/main.js
@@ -1,9 +1,47 @@
 import { films } from "../data/films.js"
+import { people } from "../data/people.js"
+import { starships } from "../data/starships.js"
 
 console.log("Hello There")
 
+
+
 const greetingDiv = document.querySelector(".greeting")
 
-greetingDiv.textcontent = "I just inserted text into a DOM element using my bad javascript skillz";
+const CASTLIST = document.createElement("ul")
 
-console.log(greetingDiv.textContent)
+let counter = 1;
+
+people.forEach(person => {
+    const listItem = document.createElement("li")
+    listItem.textContent = person.name
+    CASTLIST.appendChild(listItem)
+
+    let anchorWrap = document.createElement("a")
+    anchorWrap.href = "#"
+
+    let imageItem = document.createElement("img")
+    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    imageItem.addEventListener("click", (event) => {
+        console.log(event)
+    })
+    anchorWrap.appendChild(imageItem)
+    greetingDiv.appendChild(anchorWrap)
+    counter ++
+})
+
+let starCounter = 1;
+
+starships.forEach(ship => {
+    const listItem = document.createElement("li")
+    listItem.textContent = ship.name
+    CASTLIST.appendChild(listItem)
+
+    let imageItem = document.createElement("img")
+    imageItem.src = `https://starwars-visualguide.com/assets/img/starships/${starCounter}.jpg`
+    greetingDiv.appendChild(imageItem)
+    starCounter ++
+})
+
+greetingDiv.appendChild(CASTLIST);
+
