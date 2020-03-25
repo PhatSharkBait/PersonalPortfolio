e336582f5e8d5475f5061ef00c46529c94d82731
Cameron <Cameronjudd98@gmail.com>
2020-03-25 15:42:16 -0600

    Added CSS and printed names to main div

--- senators/main.js
+++ senators/main.js
@@ -1,11 +1,15 @@
 import {senators} from "../data/senators.js"
 
-const senatorDiv = document.querySelector('div')
-const main = document.main
+const senatorDiv = document.querySelector('.div')
+const main = document.querySelector('.main')
 
 function populatesenatorDiv(senators) {
     senators.forEach(senator => {
         console.log(`${senator.first_name} ${senator.last_name}`)
+        let name = document.createElement("h4")
+        name.textContent = `${senator.first_name} ${senator.last_name}`
+
+        main.appendChild(name)
     });
 }
 
