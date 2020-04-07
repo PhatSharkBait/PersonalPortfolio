3aeb0c3dd54fadf934b2b31a0342df9cc8f07dfa
Cameron <Cameronjudd98@gmail.com>
2020-04-07 12:36:26 -0600

    Unfinished Api Mapping

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,12 +1,51 @@
+const allPokemon = []
+
 function getPokeData(url) {
-    let pokemonGrid = document.querySelector(".pokemonGrid")
-    let pokeImg = document.querySelector(".pokeImg")
     fetch(url).then(function(response) {
-        response.json().then(function (data){
-            console.log(data)
-            pokeImg.src = data.sprites.front_default
+        response.json().then(function (pokeData){
+            const pokeMap = pokeData.results.map(pokemon => {
+                return fetch(pokemon.url).then(resData => {
+                    console.log(resData)
+                    resData.json().then(pokeJson => {
+                        allPokemon.push(pokeJson)
+                    })
+                })
+            }) 
+            console.log(pokeMap)
+            //populatePokeCards(pokeData.results)
         })
     })
 }
 
-console.log(getPokeData('https://pokeapi.co/api/v2/pokemon/1/'))
+let pokemonGrid = document.querySelector(".pokemonGrid")
+
+getPokeData('https://pokeapi.co/api/v2/pokemon?limit=25')
+
+populatePokeCards(allPokemon)
+
+
+function populatePokeCards(pokeArray) {
+    pokeArray.forEach(pokemon => {
+        let pokeScene = document.createElement('div')
+        pokeScene.className = "flip-card"
+        let pokeCard = document.createElement('div')
+        pokeCard.className = "flip-card-inner"
+        
+        let pokeFront = document.createElement('div')
+        pokeFront.className = "flip-card-front"
+        let pokeBack = document.createElement('div')
+        pokeBack.className = "flip-card-back"
+        pokeBack.textContent = pokemon.name
+    
+        pokeCard.appendChild(pokeFront)
+        pokeCard.appendChild(pokeBack)
+        pokeScene.appendChild(pokeCard)
+        pokemonGrid.appendChild(pokeScene)
+    });
+}
+
+
+/*var card = document.querySelector('.flip-card')
+card.addEventListener('click', function() {
+    card.classList.toggle('is-flipped')
+})*/
