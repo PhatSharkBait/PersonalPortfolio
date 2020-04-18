64bf21e996e6f7ebca035220ed4987ffad4e0387
PhatSharkBait <31318144+PhatSharkBait@users.noreply.github.com>
2020-04-17 19:20:39 -0600

    added progress bar code, will add sort buttons for each bar type

--- senators/main.js
+++ senators/main.js
@@ -16,7 +16,9 @@ function getSimplifiedSenators(senatorArray) {
         imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
         party:senator.party,
         seniority: parseInt(senator.seniority, 10),
-        missedVotesPct: senator.missed_votes_pct
+        missedVotesPct: senator.missed_votes_pct,
+        party: senator.party,
+        loyaltyPct: senator.votes_with_party_pct
     }
 })
 }
@@ -38,7 +40,8 @@ otherBut.addEventListener("click", (event) => {
 
 function populateSenatorDiv(simpleSenators) {
     simpleSenators.forEach(senator => {
-
+        let senCard = document.createElement('div')
+        senCard.className = "senCard"
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
@@ -48,9 +51,45 @@ function populateSenatorDiv(simpleSenators) {
 
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
-        senatorDiv.appendChild(senFigure)
+        senCard.appendChild(senFigure)
+        senCard.appendChild(progressBars(senator))
+        senatorDiv.appendChild(senCard)
     });
     main.appendChild(senatorDiv)
+}
+
+function progressBars(senator) {
+    let progressDiv = document.createElement('div')
+    progressDiv.className = 'progressDiv'
+    let loyaltyLabel = document.createElement('label')
+    loyaltyLabel.for = 'loyalty'
+    loyaltyLabel.textContent = 'Loyalty'
+    let loyaltyBar = document.createElement('progress')
+    loyaltyBar.id = 'loyalty'
+    loyaltyBar.max = 100
+    loyaltyBar.value = senator.loyaltyPct
+    let seniorityLabel = document.createElement('label')
+    seniorityLabel.for = 'seniority'
+    seniorityLabel.textContent = 'Seniority'
+    let seniorityBar = document.createElement('progress')
+    seniorityBar.id = 'seniority'
+    seniorityBar.max = mostSeniority.seniority
+    seniorityBar.value = senator.seniority
+    let votingLabel = document.createElement('label')
+    votingLabel.for = 'voting'
+    votingLabel.textContent = 'Vote'
+    let votingBar = document.createElement('progress')
+    votingLabel.id = 'voting'
+    votingBar.max = 100
+    votingBar.value = 10
+
+    progressDiv.appendChild(loyaltyLabel)
+    progressDiv.appendChild(loyaltyBar)
+    progressDiv.appendChild(seniorityLabel)
+    progressDiv.appendChild(seniorityBar)
+    progressDiv.appendChild(votingLabel)
+    progressDiv.appendChild(votingBar)
+    return progressDiv
 }
 
 const filterSenators = (prop, value) => {
